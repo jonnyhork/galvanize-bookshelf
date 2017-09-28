1e011eeea97cf45861abfe9330ccfabdce5407c2
Jonny Hork <JonnyHork@gmail.com>
2017-09-28 09:50:18 -0600

    working user routes

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt')
 const humps = require('humps')
-const knex = require('knex')
+const knex = require('../knex')
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
@@ -13,31 +13,33 @@ const router = express.Router();
 router.post('/', (req, res, next) => {
 
   let {
-    first_name,
-    last_name,
+    firstName,
+    lastName,
     email,
     password
   } = req.body
 
+  // console.log('req.body: ', req.body);
   bcrypt.hash(password, 10).then((hash) => {
 
-    knex('users')
+    return knex('users')
       .insert({
-        first_name: first_name,
-        last_name: last_name,
+        first_name: firstName,
+        last_name: lastName,
         email: email,
-        password: hash
+        hashed_password: hash
       }, '*')
       .then((user) => {
-
+        // console.log('user is:', user);
         let newUser = {
-          first_name: first_name,
-          last_name: last_name,
+          id: user[0].id,
+          first_name: firstName,
+          last_name: lastName,
           email: email
         }
-
+        // console.log('newUser with camel is:', humps.camelizeKeys(newUser));
         res.setHeader('Content-Type', 'application/json')
-        res.send(humps.camelize(newUser))
+        res.send(humps.camelizeKeys(newUser))
       })
       .catch((err) => next(err))
 
