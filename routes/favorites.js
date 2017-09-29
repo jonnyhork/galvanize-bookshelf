f3ca4a1a0015c54a15c896d2d65179debf6d18e1
Jonny Hork <JonnyHork@gmail.com>
2017-09-29 17:08:57 -0600

    post is done

--- routes/favorites.js
+++ routes/favorites.js
@@ -12,6 +12,8 @@ const SECRET = process.env.JWT_KEY
 // eslint-disable-next-line new-cap
 const router = express.Router()
 
+let currentUser
+
 // TOKEN IS CREATED WITH A USER OF user_id = 1 (userId = 1?)
 
 router.get('/', (req, res, next) => {
@@ -23,6 +25,8 @@ router.get('/', (req, res, next) => {
       }
       console.log('PAYLOAD: ', payload)
       console.log('PAYOAD ID', payload.userId);
+
+      currentUser = payload
 
       knex('favorites')
         .where('user_id', payload.userId)
@@ -42,11 +46,36 @@ router.get('/check?bookId=:id', (_req, res, next) => {
 })
 
 router.post('/', (req, res, next) => {
-  // let {}
-  knex('favorites')
-    .insert() // req.body info
 
-  // res.send() the updated stuff
+  // need to insert bookid and a user id to the favoirtes table
+  // user id will come from the req.cookies.token.
+  // book id will come from the req.body.bookId
+
+  let {
+    bookId
+  } = req.body
+
+  console.log("CURRENT USER IS: ", currentUser);
+
+  knex('favorites')
+    .insert({
+      book_id: bookId,
+      user_id: currentUser.userId
+    }, '*').then((newFavorite) => {
+
+      console.log('newFavorite== ', newFavorite);
+
+      let newObj = {
+        id: newFavorite[0].id,
+        userId: currentUser.userId,
+        bookId: newFavorite[0].book_id
+      }
+
+      res.send(newObj)
+
+    }).catch((err) => next(err))
+
+  // camelizeKeys(newFavorite)
 
 
 })
